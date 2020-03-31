00cbc357e2612be09a70992d6b0469ea34ceafd7
alexfr7 <47463015+alexfr7@users.noreply.github.com>
2020-03-31 18:45:14 -0400

    make an admin

--- ZBeans-BlazorApp/ZBeans-BlazorApp/Startup.cs
+++ ZBeans-BlazorApp/ZBeans-BlazorApp/Startup.cs
@@ -32,12 +32,14 @@ namespace ZBeans_BlazorApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddScoped<EmployeeService>();
             services.AddDbContext<StoreDbContext>(option => option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
