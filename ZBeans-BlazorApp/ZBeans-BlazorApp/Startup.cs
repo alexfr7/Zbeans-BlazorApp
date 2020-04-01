9e479b09ff2831294a9e88ef38f63145ac01c0ff
alexfr7 <47463015+alexfr7@users.noreply.github.com>
2020-03-31 21:27:51 -0400

    Worked on manager creation

--- ZBeans-BlazorApp/ZBeans-BlazorApp/Startup.cs
+++ ZBeans-BlazorApp/ZBeans-BlazorApp/Startup.cs
@@ -38,7 +38,7 @@ namespace ZBeans_BlazorApp
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
